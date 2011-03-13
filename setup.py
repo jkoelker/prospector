a28e94637ce92c0655609c05ef08ac967519e7ca
Jason Kölker <jason@koelker.net>
2011-03-13 11:31:34 -0400

    initial skel

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='prospector',
+      version=version,
+      description="Gets a mine ready",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Jason K\xc3\xb6lker',
+      author_email='jason@pickaxehosting.com',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
