650c7d80a8b7c7a52c5364878c46c27e8a8ab9bf
Jason Kölker <jason@koelker.net>
2011-03-22 20:43:09 -0500

    use the right name for yum

--- share/Fedora14/setup_python.sh
+++ share/Fedora14/setup_python.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-yum install -y PyPAM python-pip fabric twisted pyasn1 python-setuptools openssl-devel
+yum install -y PyPAM python-pip fabric twisted python-pyasn1 python-setuptools openssl-devel
 
