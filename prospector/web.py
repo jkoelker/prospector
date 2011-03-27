7839b13cd6ced1d81174f2d749c1a9ad2a001071
Jason Kölker <jason@koelker.net>
2011-03-27 14:06:31 -0500

    wire up deploy to root resource

--- prospector/web.py
+++ prospector/web.py
@@ -1,9 +1,11 @@
 from twisted.web import resource
 
 from prospector.resources import config
+from prospector.resources import deploy
 
 def getRoot():
     root = resource.Resource()
     root.putChild("config", config.getConfigResource())
+    root.putChild("deploy",  deploy.Deploy())
 
     return root
