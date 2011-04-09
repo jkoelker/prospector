f4c2266846c1028cdb9f28eac88d595050b37976
Jason Kölker <jason@koelker.net>
2011-04-09 17:55:56 -0500

    put the server jar in /usr/local

--- share/MineCraftServer/server.sh
+++ share/MineCraftServer/server.sh
@@ -4,7 +4,9 @@ BASEURL=$1
 
 yum install -y java-1.6.0-openjdk
 
-wget -q -O /home/minecraft/minecraft_server.jar ${BASEURL}/MineCraftServer/minecraft_server.jar
+mkdir -p /usr/local/lib/minecraft
 
-chown minecraft:minecraft /home/minecraft/minecraft_server.jar
+wget -q -O /usr/local/lib/minecraft/minecraft_server.jar ${BASEURL}/MineCraftServer/minecraft_server.jar
+
+#chown minecraft:minecraft /home/minecraft/minecraft_server.jar
 
