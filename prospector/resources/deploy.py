5316abe74927c546b60ac84882f1b44a6db49bac
Jason Kölker <jason@koelker.net>
2011-04-09 18:25:02 -0500

    refactor the deploy

--- prospector/resources/deploy.py
+++ prospector/resources/deploy.py
@@ -1,4 +1,5 @@
-from twisted.web import client, resource, server
+from twisted.internet import defer
+from twisted.web import client, http, resource, server
 
 from prospector import provision
 
@@ -15,7 +16,8 @@ def fail(data):
 
 class Deploy(resource.Resource):
     def render_POST(self, request):
-        def loadWorldCallback(result, data):
+        def loadWorldCallback(result, callbackUrl):
+            data = {}
             if not result:
                 fail(data)
                 return False
@@ -24,25 +26,46 @@ class Deploy(resource.Resource):
             data["shaft_port"] = 6969
             data["ssh_shaft_port"] = 2222
 
-            callbackUrl(data["callback_url"], json.dumps(data))
+            callbackUrl = data.get("callback_url", None)
+            if callbackUrl is None:
+                request.write(json.dumps(data))
+                retuest.finish()
+            else:
+                callbackUrl(data["callback_url"], json.dumps(data))
 
-        def provisionCallback(result, data):
+        def provisionCallback(result, callbackUrl, worldUrl):
             if not result:
                 fail(data)
                 return False
 
-            d = provison.loadWorld(data["world_url"])
-            d.addCallback(loadWorldCallback, data)
+            if worldUrl is not None:
+                d = provison.loadWorld(worldUrl)
+            else:
+                d = defer.Deferred()
+                d.callback(True)
+                
+            d.addCallback(loadWorldCallback, callbackUrl)
 
         data = request.content.getvalue()
-        ip = data["ip_address"]
-        hostname = data["hostname"]
-        username = data["username"]
-        password = data["password"]
-        callbackUrl = data["callback_url"]
-        worldUrl = data["world_url"]
+        ip = data.get("ip_address", None)
+        hostname = data.get("hostname", None)
+        username = data.get("username", None)
+        password = data.get("password", None)
+        callbackUrl = data.get("callback_url", None)
+        worldUrl = data.get("world_url", None)
+
+        if ip is None or username is None or password is None:
+            return resource.ErrorPage(http.BAD_REQUEST,
+                                      http.RESPONSES[http.BAD_REQUEST],
+                                      "ip_address, username, and password " +
+                                      "required")
+
 
         d = provision.provision(ip, username, password)
-        d.addCallback(provisionCallback, data)
+        d.addCallback(provisionCallback, callbackUrl, worldUrl)
+
+        if callbackUrl is None:
+            return server.NOT_DONE_YET
+
         return data
 
