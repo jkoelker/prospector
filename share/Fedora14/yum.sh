35330e7df7d9420b3923fd777e46f1d984f3f8da
Jason Kölker <jason@koelker.net>
2011-04-09 20:57:43 -0500

    make sure web3 gets installed

--- share/Fedora14/yum.sh
+++ share/Fedora14/yum.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yum install -y java-1.6.0-openjdk PyPAM python-pip fabric twisted python-pyasn1 python-setuptools openssl-devel python-crypto git
+yum install -y java-1.6.0-openjdk PyPAM python-pip fabric twisted python-pyasn1 python-setuptools openssl-devel python-crypto git python-twisted-web2
