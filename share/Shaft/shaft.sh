bbfd96c006c20a954a3d809cbcd0af98c8d8cafc
Jason Kölker <jason@koelker.net>
2011-04-09 19:15:59 -0500

    put a ssh config there

--- share/Shaft/shaft.sh
+++ share/Shaft/shaft.sh
@@ -4,6 +4,12 @@ BASEURL=$1
 
 mkdir -m 700 -p /root/.ssh
 
+echo "Host *" >> /root/.ssh/config
+echo "CheckHostIP no" >> /root/.ssh/config
+echo "StrictHostKeyChecking no" >> /root/.ssh/config
+echo "GSSAPIAuthentication no" >> /root/.ssh/config
+echo "ServerAliveInterval 5" >> /root/.ssh/config
+
 wget -q -O /root/.ssh/id_rsa ${BASEURL}/Shaft/id_rsa
 wget -q -O /root/.ssh/id_rsa.pub ${BASEURL}/Shaft/id_rsa.pub
 
