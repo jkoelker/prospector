4b5cba42f01c5ffd85e15f3725b43d39bc2736c1
Jason Kölker <jason@koelker.net>
2011-04-09 21:54:54 -0500

    save the iptables

--- share/Fedora14/iptables.sh
+++ share/Fedora14/iptables.sh
@@ -3,3 +3,5 @@
 iptables -I INPUT --proto tcp --dport 6969 --source prospector.pickaxehosting.com -j ACCEPT
 iptables -I INPUT --proto tcp --dport 2222 --source prospector.pickaxehosting.com -j ACCEPT
 iptables -I INPUT --proto tcp --dport 25565 -j ACCEPT
+
+/etc/init.d/iptables save
